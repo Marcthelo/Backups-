a1c18a63a248ef08c8cff804c16c1f309cea841e
MarceloSJ <133117404+Marcthelo@users.noreply.github.com>
2024-03-17 21:41:02 -0300

    Add files via upload

---
+++ TCC/conexao.php
@@ -0,0 +1,14 @@
+<?php 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "rsp_banco";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+    //echo "Funcionou";
+} catch (PDOException $err) {
+    echo "Erro";
+    $err->getMenssage();
+}
+?>
