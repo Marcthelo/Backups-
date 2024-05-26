4da4450409ee92849f0cb88ec7ad1808e0d272ec
MarceloSJ <133117404+Marcthelo@users.noreply.github.com>
2024-05-26 19:56:39 -0300

    Update FormProdutos.php

--- TCC2024-05-02/php-server/FormulariosLogin/FormProdutos.php
+++ TCC2024-05-02/php-server/FormulariosLogin/FormProdutos.php
@@ -1,6 +1,83 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+        <style>
+        a{
+            text-decoration: none;
+            color: rgb(255, 0, 0);
+        }
+
+        body {
+            background-color:#818181;
+            background-image: url("../../imgs/fundo1.png");
+            background-size: cover;
+        }
+
+        fieldset {
+            background-color: #B40000;
+            border: solid #FF0000;
+            border-radius: 9%;
+            border-width: 20px;
+            width: 15%;
+            height: 60%;
+            align-items: center;
+            margin-left: 40%;
+           margin: auto;
+        }
+ 
+        .inputBox {
+            display: inline;
+        }
+
+        input {
+            background-color: #FFFFFF;
+            border-radius: 9%;
+            margin-left: 2%;
+            border-color: #FFFFFF;
+            margin-left: 18%;
+           
+        }
+
+        label {
+            color: #f0f0f0;
+            text-decoration: bold;
+            margin: auto;
+        
+            margin-top: 0%;
+            border-radius: 30%;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        }
+
+        button {
+            color: #ff0000;
+            border-radius: 9%;
+            margin-left: 20%
+            
+        }
+ 
+        legend {
+            margin-top: 10%;
+            color: #ffffff;
+            border-radius: 30%;
+            margin-left: 20%
+        }
+        #botoes {
+            display: flex;
+        }
+
+        p {
+            color: #9F9F9F;
+            border-radius: 30%;
+            font-size: 30%;
+            margin-left: 2%
+        }
+        #button {
+            color: #ff0000;
+            border-radius: 9%;
+            margin-left: 20%
+            
+        }
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produto</title>
@@ -48,8 +125,12 @@
                     <label for="fab">Fabricante</label>
                     <input type="text" name="fab" id="fab" class="inputProduto" required />
                 </div>
-                <br>
-                <input type="submit" name="submit" id="submit" value="cadastrar" />
+                <br> <br>
+            <div id="btn">
+                <input id="button" name="submit" type="submit" value="Cadastrar">
+               <br><br>
+                <a href="../php-server/Paginas/IndexPrincipal.php"><input id="button" type="submit" value="Voltar"></a>
+                </div>
         </form>
     </fieldset>
 </body>
