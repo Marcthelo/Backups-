9039c2cdd7561e997bbf5f9ee65150ba0d5c4482
MarceloSJ <133117404+Marcthelo@users.noreply.github.com>
2024-06-30 18:47:57 -0300

    Add files via upload

---
+++ TCC-30-06-2024/banco/BANCO_RSP-30-06-2024.sql
@@ -0,0 +1,231 @@
+CREATE DATABASE  IF NOT EXISTS `rsp_banco` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `rsp_banco`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: rsp_banco
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adm`
+--
+
+DROP TABLE IF EXISTS `adm`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `adm` (
+  `idadm` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_usuario` enum('cliente','empresa','adm') NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `endereco` varchar(45) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado_id` int(11) NOT NULL,
+  `cidade_id` int(11) NOT NULL,
+  PRIMARY KEY (`idadm`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adm`
+--
+
+LOCK TABLES `adm` WRITE;
+/*!40000 ALTER TABLE `adm` DISABLE KEYS */;
+INSERT INTO `adm` VALUES (1,'adm','celos','987654','324','celos@gmail.com','av.cascavel','SE','santo amaro',0,0),(2,'adm','celos','987654','324','celos@gmail.com','av.cascavel','SE','santo amaro',0,0),(3,'adm','Marcelo Silva Junior','3','5','marcelo@gmail.com','CASA','RS','campinas',0,0),(4,'adm','Marcelo Silva Junior','3','5','marcelo@gmail.com','CASA','RS','campinas',0,0);
+/*!40000 ALTER TABLE `adm` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cidade`
+--
+
+DROP TABLE IF EXISTS `cidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cidade` (
+  `id_cidade` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeCidade` int(11) NOT NULL,
+  PRIMARY KEY (`id_cidade`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cidade`
+--
+
+LOCK TABLES `cidade` WRITE;
+/*!40000 ALTER TABLE `cidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cidade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `idcliente` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_usuario` enum('cliente','empresa','adm') NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `telefone` varchar(45) NOT NULL,
+  `endereco` varchar(45) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado_id` int(11) NOT NULL,
+  `cidade_id` int(11) NOT NULL,
+  PRIMARY KEY (`idcliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'cliente','bianca','1654861','123','binca@gmail.com','2024-04-29','55675436','casadokrl','PR','cascavel',0,0),(2,'cliente','ABN','3','56','ABN@YAHOO.COM','2000-02-10','19','AV.BRASIL','PB','FORTAELZA',0,0),(3,'cliente','marcelo','085432','736','marceloSJ@gmail.com','2004-07-29','45998135467','Pacaembu','PR','cascavel',0,0),(5,'cliente','Andre','28976','43','andre@gmail.com','1995-11-14','5476536298','Pacaembu','PR','Cascavel',0,0);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empresa`
+--
+
+DROP TABLE IF EXISTS `empresa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empresa` (
+  `idempresa` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_usuario` enum('cliente','empresa','adm') NOT NULL,
+  `razao_social` varchar(45) NOT NULL,
+  `cnpj` varchar(14) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `telefone` varchar(45) NOT NULL,
+  `endereco` varchar(45) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado_id` int(11) NOT NULL,
+  `cidade_id` int(11) NOT NULL,
+  PRIMARY KEY (`idempresa`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empresa`
+--
+
+LOCK TABLES `empresa` WRITE;
+/*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
+INSERT INTO `empresa` VALUES (1,'empresa','casa Do bahiano','189456few','456','casa@bainhao.com','5431','casa','RN','pelourinho',0,0);
+/*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estado`
+--
+
+DROP TABLE IF EXISTS `estado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estado` (
+  `id_estado` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeEstado` int(11) NOT NULL,
+  PRIMARY KEY (`id_estado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estado`
+--
+
+LOCK TABLES `estado` WRITE;
+/*!40000 ALTER TABLE `estado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido` (
+  `codigo_id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` int(11) NOT NULL,
+  `dataProduto` date NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  PRIMARY KEY (`codigo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `id_produto` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_produto` enum('CARRO','TRATOR','CAMINHÃO','MOTO','MOTONETA') DEFAULT NULL,
+  `img_produto` mediumblob DEFAULT NULL,
+  `descricao` varchar(220) DEFAULT NULL,
+  `tipo_fabricante` varchar(200) DEFAULT NULL,
+  `peca` enum('Induzido','Estator','Rotor','Bobina','Automático','Impulsor','Chaves de setas') DEFAULT NULL,
+  `qtd_volts` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (22,'',_binary 'C:xampphtdocsTCC-29-06-2024ancoBanco_imgs6681cf64ef05629_abcmotoneta.jpg','motoneta guidão','WAPSA','',13),(23,'',_binary 'C:xampphtdocsTCC-29-06-2024ancoBanco_imgs6681cfaf57961EXTENSION.trator.jpg','trator foda','MAGNETI_MARELI','Bobina',41);
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-30 18:43:45
