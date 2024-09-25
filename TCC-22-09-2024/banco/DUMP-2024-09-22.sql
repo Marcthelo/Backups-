38e42d3b260e74643c02e8d96a6198ab24dbd996
MarceloSJ <133117404+Marcthelo@users.noreply.github.com>
2024-09-25 07:28:29 -0300

    Update DUMP-2024-09-22.sql

--- TCC-22-09-2024/banco/DUMP-2024-09-22.sql
+++ TCC-22-09-2024/banco/DUMP-2024-09-22.sql
@@ -156,8 +156,11 @@ UNLOCK TABLES;
 
 --
 -- Table structure for table `usuarios`
---
 
+
+
+
+-- APAGAR DATA DE NASCIMENTO
 DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
